ca7a5cb78a393effb93900b160a9d63d6f674c7d
Su-mia <soumia.bouaouina@ensia.edu.dz>
2024-05-11 01:21:50 +0100

    updates

--- app/views.py
+++ app/views.py
@@ -10,37 +10,47 @@ def leaderboard_view(request):
     """
 
     teams_first = [
-        "kurtosis",
-        "Megatron",
-        "PentechAI",
-        "CrusAIders",
-        "Vgaith",
-        "K-beans",
-        "idk"
+          "Myss AI",
+          "ML Builders",
+          "zero"
+
     ]  # Replace with actual team names
     competitions_first = [
-        "instadeep-challenge-soai",
-        "iiot-cyber-security-challenge-haick-2022",
-        # "jumia-purchase-prediction",
-        # "lcbm-challenge-haick-2022",
+        "image-classification-aifest24",
+        "multi-label-classification-aifest24",
+        "cars-price-prediction"
     ]  # Replace with actual competition IDs
-    weights_first = [30 , 70]  # Replace with actual weights
+    weights_first = [6 , 6 , 5]  # Replace with actual weights
+
+    additionalList = {
+    'kurtosis': { 'not-in-kaggle' : {'rank': 2, 'inCompetition': True}}, 
+    'Megatron': { 'not-in-kaggle' : {'rank': 1, 'inCompetition': True}}, 
+    }
 
     # get the global rank of the first set of teams
-    global_rank_first = calculate_global_rank(teams_first, competitions_first, weights_first)
+    global_rank_first = calculate_global_rank(teams_first, competitions_first, weights_first , False , {})
 
     teams_second = [
-         "kurtosis",
-        "Megatron",
-        "PentechAI",
+         "ABCode",
+         "LuminAi",
+         "HyperML",
+         "0v3r_fl0w",
+         "BONSWAYNAGUI",
+         "Data Seekers",
+         "TeamOne",
+         "the brainiacs",
     ]  # Replace with actual team names
     competitions_second = [
-        "instadeep-challenge-soai",
+       "help-djelloul-to-grow-his-business-classification",
+       "ai-fest-smurfs-challenge",
+       "risky-patterns-in-io-t-device-interactions",
+       "space-aifest24",
+       "the-boss",
     ]  # Replace with actual competition IDs
-    weights_second = [100]  # Replace with actual weights
+    weights_second = [4 , 6 , 6, 5, 5]  # Replace with actual weights
 
     # get the global rank of the second set of teams
-    global_rank_second = calculate_global_rank(teams_second, competitions_second, weights_second)
+    global_rank_second = calculate_global_rank(teams_second, competitions_second, weights_second , False , {})
 
     # create the context
     context = {
